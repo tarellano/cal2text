1a3752b482bb6fe97dae8626999bcfecea338551
tarellano <thomas.g.arellano@gmail.com>
2024-06-27 15:30:45 -0400

    Add create and delete event handlers

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,15 +1,45 @@
-import { useRef } from "react";
-import { DayPilotCalendar, DayPilotNavigator } from "@daypilot/daypilot-lite-react";
+import { useRef, useState } from "react";
+import { DayPilot, DayPilotCalendar, DayPilotNavigator, CalendarProps } from "@daypilot/daypilot-lite-react";
 
 const Calendar = () => {
 
     const calendarRef = useRef<DayPilotCalendar>(null);
 
     const handleTimeRangeSelected = (args: { day: any; }) => {
-        calendarRef.current?.control.update({
+        calendarRef.current!.control.update({
             startDate: args.day
         });
     }
+
+    const handleNewEvent = async (args: { start: any; end: any; }) => {
+        const dp = calendarRef.current!.control;
+        dp.clearSelection();
+        dp.events.add({
+          start: args.start,
+          end: args.end,
+          id: DayPilot.guid(),
+          text: "free"
+        });
+    }
+
+    const handleDeleteEvent = async (args: { source: DayPilot.Event; }) => {
+        const dp = calendarRef.current!.control;
+        dp.events.remove(args.source);
+    }
+
+    const [calendarConfig, setCalendarConfig] = useState<CalendarProps>({
+        viewType: "Week",
+        timeRangeSelectedHandling: "Enabled",
+        onTimeRangeSelected: handleNewEvent,
+        contextMenu: new DayPilot.Menu({
+            items: [
+              {
+                text: "Delete",
+                onClick: handleDeleteEvent
+              }
+            ]
+          }),
+    });
 
     return (
         <div>
@@ -17,7 +47,7 @@ const Calendar = () => {
                 selectMode="Week"
                 onTimeRangeSelected={handleTimeRangeSelected}
             />
-            <DayPilotCalendar viewType="Week" durationBarVisible={false} ref={calendarRef}/>
+            <DayPilotCalendar {...calendarConfig} ref={calendarRef}/>
         </div>
     )
 }
