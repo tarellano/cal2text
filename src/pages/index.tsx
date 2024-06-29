b1eff5146138045703eb31a6a208a7c8d8243132
tarellano <thomas.g.arellano@gmail.com>
2024-06-28 22:54:08 -0400

    Fix imports in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import { DayPilotCalendar } from "@daypilot/daypilot-lite-react";
-import Calendar from "./components/Calendar";
 import Cal2Text from "./components/Cal2Text";
 
 const inter = Inter({ subsets: ["latin"] });
